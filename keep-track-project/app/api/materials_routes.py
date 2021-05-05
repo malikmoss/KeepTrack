9cfe6fdd9a0e199d5c49e41a83d1b6e9daab0f85
Malik Moss <malikmoss33@gmail.com>
2021-05-05 17:54:45 -0500

    working on materials backend routes

--- keep-track-project/app/api/materials_routes.py
+++ keep-track-project/app/api/materials_routes.py
@@ -7,4 +7,7 @@ mat_routes = Blueprint('materials', __name__)
 @mat_routes.route('/')
 @login_required
 def get_materials():
-    
+    userId = int(current_user.id)
+    userMaterials = Material.query.filter(Material.user_id == user_id).all()
+    materialIds = [userMaterials.material_id for userMaterials in userMaterials]
+    materials = [material.to_dict() for server in raw_materials]
