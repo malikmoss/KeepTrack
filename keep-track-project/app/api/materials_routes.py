fe51fbfa7c7dc725b11649ca083e9bb7b779867b
Malik Moss <malikmoss33@gmail.com>
2021-05-05 18:17:58 -0500

    working on addMaterial route - review getAllMaterials route

--- keep-track-project/app/api/materials_routes.py
+++ keep-track-project/app/api/materials_routes.py
@@ -4,10 +4,13 @@ from flask_login import current_user, login_required
 
 mat_routes = Blueprint('materials', __name__)
 
+#looking to retrieve all materials
 @mat_routes.route('/')
 @login_required
 def get_materials():
     userId = int(current_user.id)
     userMaterials = Material.query.filter(Material.user_id == user_id).all()
-    materialIds = [userMaterials.material_id for userMaterials in userMaterials]
-    materials = [material.to_dict() for server in raw_materials]
+    # materials = [material.to_dict() for material in raw_materials]
+    return {'materials' : materials}
+
+@mat_routes('/', methods=['POST'])
